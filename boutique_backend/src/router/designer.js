15bc76d03248cdce65383cfa8151476ba7d046df
clarencepraison <clarencepraison32303@gmail.com>
2024-08-31 11:07:06 +0530

    Assortment

--- boutique_backend/src/router/designer.js
+++ boutique_backend/src/router/designer.js
@@ -1,10 +1,14 @@
 const express           = require('express');
 const router            = express.Router();
+
+//First Call the file here 
 const material_category = require('../designer/material_category');
 const dress_category    = require('../designer/dress_category');
 const product           = require('../designer/add_product');
-const gender           = require('../designer/gender');
+const gender            = require('../designer/gender');
+const productAssortment = require('../designer/product_assortment');
 
+// Next Add the function here
 //Material Category
 router.post('/add_material_category', material_category.addCategory);
 router.post('/update_material_category', material_category.updateCategory);
@@ -20,6 +24,14 @@ router.get('/get_dress_category', dress_category.getCategory);
 //Gender Category
 router.post('/add_gender', gender.addGender);
 router.post('/update_gender', gender.updateGender);
+router.post('/delete_gender', gender.deleteGender);
+router.post('/read_gender', gender.getGender);
+
+//Product Assortment Category
+router.post('/add_assortment', productAssortment.addAssortment);
+router.post('/update_assortment', productAssortment.updateAssortment);
+router.post('/delete_assortment', productAssortment.deleteAssortment);
+
 
 
 //Add Product
